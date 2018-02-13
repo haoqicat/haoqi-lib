d2fe634b9d67718770f75a19a5cab8c958f66d27
Peter Wang <happypeter1983@gmail.com>
2018-02-13 15:25:33 +0800

    createPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,9 @@
-exports.createPages = () => {
-  console.log('createPages....')
+const path = require(`path`)
+
+exports.createPages = ({ boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  createPage({
+    path: '001-setup',
+    component: path.resolve(`./src/templates/episode.js`)
+  })
 }
---
+++ src/templates/episode.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => {
+  return <div>模板文件 Episode</div>
+}
