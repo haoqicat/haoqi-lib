6ec7090cf75095ddbf6a871a6c736d44154cc806
Peter Wang <happypeter1983@gmail.com>
2018-02-13 16:29:24 +0800

    pkg

--- src/templates/episode.js
+++ src/templates/episode.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-export default ({ pathContext }) => {
-  const { episode } = pathContext
-  return <div>{episode}</div>
+export default ({ data }) => {
+  const ep = data.markdownRemark
+  return <div dangerouslySetInnerHTML={{ __html: ep.html }} />
 }
+
+export const query = graphql`
+  query PageQuery($episode: String!) {
+    markdownRemark(fields: { episode: { eq: $episode } }) {
+      html
+    }
+  }
+`
