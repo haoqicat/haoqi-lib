282be5f04d3f64a5a9a692b2d37a684893614093
Peter Wang <happypeter1983@gmail.com>
2018-02-13 15:07:47 +0800

    styled-components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../assets/global.css'
+import styled from 'styled-components'
+import { PRIMARY } from '../constants/Colors'
 
 export default ({ children }) => {
   return (
     <div>
-      LAYOUT
+      <Header />
       {children()}
     </div>
   )
 }
+
+const Header = styled.div`
+  height: 80px;
+  background: ${PRIMARY};
+`
